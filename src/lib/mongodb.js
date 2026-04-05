c411fdf69571df23d686c84d0d81bbf1dce6b0d3
gudigarrohit <gudigarrohit@gmail.com>
2026-04-05 16:25:00 +0530

    dlt

--- src/lib/mongodb.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const MONGO_URI = process.env.MONGODB_URI;
-
-export async function connectDB() {
-  if (mongoose.connection.readyState >= 1) return;
-
-  return mongoose.connect(MONGO_URI);
-}
